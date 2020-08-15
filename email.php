5515954ab164b4eb3cb267354b6ee359e18951e4
leolima21 <leo.lima21051990@gmail.com>
2020-08-15 15:19:35 -0300

    34

--- email.php
+++ email.php
@@ -4,13 +4,14 @@
   $email = addslashes($_POST["email"])
   $mensagem = addslashes($_POST["message"])
 
-  $to = "leo.mendes21@hotmail.com";
+  $to = "harmonycortinas@gmail.com";
   $subject = "contato - Harmony decoracoes";
 
-  $body = "Nome: " .$nome "\n".
-          "Email: " .$email "\n". 
-          "Mensagem: " .$mensagem "\n";
-          
+  $body = "Nome: ".$nome."\r\n".
+          "Email: ".$email."\r\n". 
+          "Mensagem: ".$mensagem;
+   
+  // o email do from tem que ser o email do host
   $header = "From: tucca@harmonydeco.com"."\r\n".
             "Reply-To:".$email."\e\n".
             "X=Mailer:PHP/".phpversion();
